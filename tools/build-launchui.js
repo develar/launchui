8529ae1342c92ed884df42483283b1c75cb26508
develar <develar@gmail.com>
2018-11-17 08:08:32 +0100

    use getNodeModuleVersion instead of readdir

--- tools/build-launchui.js
+++ tools/build-launchui.js
@@ -1,6 +1,7 @@
 const path = require( 'path' );
 const fs = require( 'fs' );
 const child_process = require( 'child_process' );
+const { getNodeModuleVersion } = require( './utils' );
 
 if ( process.platform == 'win32' )
   buildLaunchUIWin32( process.env.npm_config_arch || process.arch );
@@ -28,19 +29,17 @@ function buildLaunchUI() {
   const nodeDir = path.join( __dirname, '../deps/node' );
   const libuiDir = path.join( __dirname, '../deps/libui' );
 
-  let isMacOs = process.platform === 'darwin';
+  const isMacOs = process.platform === 'darwin';
   const dir = isMacOs ? 'out/Release' : 'out/Release/lib.target';
-  const file = fs.readdirSync(path.join(nodeDir, dir)).filter(it => it.startsWith("libnode."))[0]
-  const nodeSrc = path.join(nodeDir, dir, file);
-  const nodeDest = path.join(buildDir, isMacOs ? 'libnode.dylib' : 'libnode.so');
-  if ( !fs.existsSync( nodeDest ) )
-    fs.symlinkSync( nodeSrc, nodeDest );
+  const file = isMacOs ? `libnode.${getNodeModuleVersion()}.dylib` : `libnode.so.${getNodeModuleVersion()}`;
+  const nodeSrc = path.join( nodeDir, dir, file );
+  const nodeDest = path.join( buildDir, isMacOs ? 'libnode.dylib' : 'libnode.so' );
+  symlinkIfNotExist( nodeSrc, nodeDest );
 
   const libuiSrc = path.join( libuiDir, isMacOs ? 'build/out/libui.A.dylib' : 'build/out/libui.so.0' );
   const libuiDest = path.join ( buildDir, isMacOs ? 'libui.dylib' : 'libui.so' );
 
-  if ( !fs.existsSync( libuiDest ) )
-    fs.symlinkSync( libuiSrc, libuiDest );
+  symlinkIfNotExist( libuiSrc, libuiDest );
 
   let cmakePath = 'cmake';
   if ( isMacOs && fs.existsSync( '/Applications/CMake.app/Contents/bin/cmake' ) )
@@ -55,4 +54,14 @@ function buildLaunchUI() {
 
   if ( result.error != null )
     throw result.error;
+}
+
+function symlinkIfNotExist( src, dest ) {
+  try {
+    fs.symlinkSync( src, dest );
+  } catch ( e ) {
+    if ( e.code !== "EEXIST" ) {
+      throw e
+    }
+  }
 }
